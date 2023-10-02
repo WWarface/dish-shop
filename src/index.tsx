0f03eecb3e2ed44af52128dea290d87af0d1e796
Haretone Shulge <2003harik2003@gmail.com>
2023-10-02 20:15:39 +0300

    Added Provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { reduxStore } from "./redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
