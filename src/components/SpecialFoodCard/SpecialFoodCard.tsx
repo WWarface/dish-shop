4f539230e5fb9b85ae950e361c43a050a64c3fe5
Oleg <matyiokin2002@gmail.com>
2023-10-06 17:32:24 +0300

    Added dashed border to SpecialFoodCard (maybe it would be better to use background as dashed border, but for now simple border is enough)...

--- src/components/SpecialFoodCard/SpecialFoodCard.tsx
+++ src/components/SpecialFoodCard/SpecialFoodCard.tsx
@@ -14,12 +14,18 @@ interface SpecialFoodCardProps{
 const SpecialFoodCard:FC<SpecialFoodCardProps> = ({title,slogan,buttonValue,specialImage, secondaryImage, text}) => {
     return (
         <div className={cl.card}>
-            <div className={cl.card__imageContainer}>
-                <span className={cl.card__specialText}>Today's Special</span>
-                <img className={cl.card__mainImg} src={specialImage} alt=""/>
-                <div className={cl.card__secondImgContainer}>
-                    <img className={cl.card__secondImg} src={require(`../../assets/images/${secondaryImage}`)} alt=""/>
+            <div className={cl.card__imageWrapper}>
+                <div className={cl.card__imageContainer}>
+                    <span className={cl.card__specialText}>Today's Special</span>
+                    <img className={cl.card__mainImg} src={require(`../../assets/images/${specialImage}`)} alt=""/>
+                    <div className={cl.card__secondImageWrapper}>
+                        <div className={cl.card__secondImgContainer}>
+                            <img className={cl.card__secondImg} src={require(`../../assets/images/${secondaryImage}`)} alt=""/>
+                        </div>
+                        <div className={cl.card__secondImageDashedBorder}></div>
+                    </div>
                 </div>
+                <div className={cl.card__dashedBorder}></div>
             </div>
             <div className={cl.card__info}>
                 <p className={cl.card__title}>{title}</p>
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -16,7 +16,7 @@ const Home = () => {
                 slogan={'We Speak The Good For Language'}
                 buttonValue={'Learn More'}
                 text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid asperiores assumenda dolorem doloribus magnam minima.'}
-                specialImage={"https://dishshop.adaptable.app/image/f7f91549-9801-4cc1-8028-721495f5ff3e.jpg"}
+                specialImage={'dumplings.png'}
                 secondaryImage={'dumplings.png'}
             />
         </div>
